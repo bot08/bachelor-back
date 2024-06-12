c4701d4f1fdfc858f225dcfba261992272a6d0d5
Oleg <scherbaoleg4@gmail.com>
2024-06-12 16:12:38 +0300

    frames images

--- sql-raw/frames.sql
+++ sql-raw/frames.sql
@@ -1,7 +1,7 @@
 INSERT INTO Frames (FrameManufacturer, FrameName, FrameImage, FrameDescription, FramePrice) 
 VALUES 
-    ('Ray-Ban', 'Wayfarer', 'wayfarer.jpg', 'Класична оправа Wayfarer від Ray-Ban.', 1200.00),
-    ('Oakley', 'Frogskins', 'frogskins.jpg', 'Стильна оправа Frogskins від Oakley.', 1400.00),
-    ('Persol', 'PO3019S', 'po3019s.jpg', 'Елегантна оправа PO3019S від Persol.', 1800.00),
+    ('Ray-Ban', 'Wayfarer', '/database/images/oprava1.jpg', 'Класична оправа Wayfarer від Ray-Ban.', 1200.00),
+    ('Oakley', 'Frogskins', '/database/images/oprava2.jpg', 'Стильна оправа Frogskins від Oakley.', 1400.00),
+    ('Persol', 'PO3019S', '/database/images/oprava3.jpg', 'Елегантна оправа PO3019S від Persol.', 1800.00),
     ('Maui Jim', 'Bamboo Forest', 'bamboo_forest.jpg', 'Спортивна оправа Bamboo Forest від Maui Jim.', 1600.00),
     ('Prada', 'PR 17WS', 'pr_17ws.jpg', 'Модна оправа PR 17WS від Prada.', 2000.00);
